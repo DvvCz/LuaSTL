ce5c09d717c67926bda59fe57fb1a1ef1559802c
DvvCz <86097860+DvvCz@users.noreply.github.com>
2023-02-15 23:05:56 -0800

    Implement http.get for unix with cURL
    Haven't tested but should work fine.
    
    Fixes #1

--- std/net/http/mod.lua
+++ std/net/http/mod.lua
@@ -1,18 +1,40 @@
+local env = require "std.env.mod"
+
 local http = {}
+
+local internal_get
+if env.consts.OS == "Unix" then
+	function internal_get(url)
+		assert(type(url) == "string", "Expected string url for http.get argument #1")
+
+		local handle = io.popen(string.format("curl %q", url), "r")
+		if not handle then return end
+
+		local out = handle:read("*a")
+		handle:close()
+
+		return out
+	end
+else
+	function internal_get(url)
+		assert(type(url) == "string", "Expected string url for http.get argument #1")
+
+		local handle = io.popen(string.format("powershell -Command (Invoke-WebRequest %q).Content", url), "r")
+		if not handle then return end
+
+		local out = handle:read("*a")
+		handle:close()
+
+		return out
+	end
+end
 
 --- Synchronous http request
 ---@param url string
 ---@return string?
 function http.get(url)
 	assert(type(url) == "string", "Expected string url for http.get argument #1")
-
-	local handle = io.popen("powershell -Command (Invoke-WebRequest " .. url .. ").Content", "r")
-	if not handle then return end
-
-	local out = handle:read("*a")
-	handle:close()
-
-	return out
+	return internal_get(url)
 end
 
 return http
